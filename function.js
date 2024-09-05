6257c7160e55182542a983b7080b83acba296735
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-09-06 00:56:11 +0600

    Js function file added and js function code done.

---
+++ function.js
@@ -0,0 +1,15 @@
+// Function declare---------------->>
+function FanOffKor() {
+  console.log("Bed theke uthe.");
+  console.log("Switch er dike ja.");
+  console.log("Switch ta off kor.");
+}
+// function call------>>>
+FanOffKor();
+
+function Mathbook() {
+  console.log("Ghum theke uth.");
+  console.log("Uthe math korte bos.");
+}
+// function call --------->>>
+Mathbook();
